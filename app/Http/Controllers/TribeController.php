052969a174ed1324975912cb73fc6eb0677b3c79
Klimbar <chengklim179@gmail.com>
2026-05-08 11:57:23 +0530

    Add controllers and models for Seven Sisters Wear (Product, Seller, State, Tribe)

---
+++ app/Http/Controllers/TribeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tribe;
+use Illuminate\Http\Request;
+
+class TribeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tribe $tribe)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tribe $tribe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tribe $tribe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tribe $tribe)
+    {
+        //
+    }
+}
